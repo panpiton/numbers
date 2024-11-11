9d45fcb44393ddacbd9e6ea48725441fd3b4fbf2
Pavel <138117566+panpiton@users.noreply.github.com>
2024-11-11 21:34:03 +0300

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -1,21 +1,22 @@
-mnum = 0
-msum = 0
+mnum = 0 
+msum = 0 
 counter = 0
-while True:
+while True: 
     try:
         n = int(input('Введи целое число: ')) 
     except ValueError:
         print('Введенный символ не является целым числом')
-        continue
-    if n == 0:
-        break
-    m = n
-    s = 0
-    while n:
-        s += n % 10
-        n //= 10
-    if s > msum:
-        mnum = m
-        msum = s
+        continue    
+    if n == 0: 
+        break 
+    m = n 
+    s = 0 
+    counter += 1
+    while n: 
+        s += n % 10 
+        n //= 10 
+    if s > msum: 
+        mnum = m 
+        msum = s 
 if counter > 0:
-    print(f'Число с максимальной суммой цифр {mnum} Сумма цифр этого числа {msum}')
+    print('Число с максимальной суммой цифр', mnum, 'Сумма цифр этого числа', msum)
